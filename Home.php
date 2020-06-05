47a0396212de044dd10c5be9051d740a850d2bc5
phantom70 <62509497+phantom70@users.noreply.github.com>
2020-06-05 20:22:47 +0200

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<title>Fruit && Herbs online shopping</title>
+<html>
+<head>
+  <link rel='stylesheet' href='Admin/css/bootstrap.css'/>
+  <link rel='stylesheet' href='Admin/css/style.css'/>
+
+</head>
+<body>
+  <div class="imgback">
+  <img src="Admin/imges/back.png" alt="background" class="img">
+</div>
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+
+        <a class="navbar-brand" href="#">Home</a>
+          <a class="navbar-brand" href="aboutus.php">About us</a>
+            <a class="navbar-brand" href="register.php">Register</a>
+              <a class="navbar-brand" href="login.php">Login</a>
+      </div>
+    </div>
+  </nav>
+  <div>
+    <img src="Admin/imges/fruit.png" class="fruit">
+    <img src="Admin/imges/herb.png"  class="herbs">
+
+  </div>
+  <div>
+      <h3>   Have look at out beautiful farm</h3>
+    <p>
+We started Warsaw Organic Store in order to source organic produce directly from local farms.<br>
+The year was 2012. It was the best of times, it was the worst of times.<br>
+Our Goal: The goal was (and still is) to provide a service where local farms<br>
+ are given priority and members are able to access the freshest organic items available at a fair price.<br>
+We’ve spent the past few years building a business and service that we love. A service that makes the safest
+</p>
+    <img src="Admin/imges/food.png" alt="food" class="foodhom">
+    <img src="Admin/imges/roman.png" alt="food" class="fodhom">
+  </div>
+
+<!---
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">About us <span class="sr-only">(current)</span></a></li>
+          <li><a href="#">connect us</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Login</a></li>
+              <li><a href="#">Register</a></li>
+
+            </ul>
+          </li>
+        </ul>
+        <form class="navbar-form navbar-left">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  --->
+
+
+
+  <script src="js/jquery-1.12.0.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
