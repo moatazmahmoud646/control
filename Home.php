2646f6f14f45c178190223fe05db8e61dfbdd3c6
phantom70 <62509497+phantom70@users.noreply.github.com>
2020-06-05 19:49:11 +0200

    Add files via upload

---
+++ Home.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta charset="utf-8">
+<title>Online Auction</title>
+<html>
+<head>
+  <link rel='stylesheet' href='Admin/css/bootstrap.css'/>
+  <link rel='stylesheet' href='Admin/css/style.css'/>
+
+</head>
+<body>
+  
+  <nav class="navbar navbar-inverse">
+    <div class="container-fluid">
+      <!-- Brand and toggle get grouped for better mobile display -->
+      <div class="navbar-header">
+
+        <a class="navbar-brand" href="#">Home</a>
+          <a class="navbar-brand" href="aboutus.php">About us</a>
+            <a class="navbar-brand" href="register.php">Register</a>
+              <a class="navbar-brand" href="login.php">Login</a>
+      </div>
+    </div>
+  </nav>
+  <div>
+    <img src="Admin/imges/2.jpg" class="fruit">
+   
+
+  </div>
+  <div>
+      <h3>  An online auction project</h3>
+    <p>
+that holds online
+auctions of various products <br>
+on a website and serves sellers and bidders accordingly.<br>
+The project is designed to allow users to set up
+their products <br>
+for auctions and bidders to
+register and bid for various <br>
+products available
+for bidding.
+</p>
+    <img src="Admin/imges/1.jpg" alt="food" class="foodhom">
+    <img src="Admin/imges/3.jpg" alt="food" class="fodhom">
+  </div>
+
+<!---
+
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <li class="active"><a href="#">About us <span class="sr-only">(current)</span></a></li>
+          <li><a href="#">connect us</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Login</a></li>
+              <li><a href="#">Register</a></li>
+
+            </ul>
+          </li>
+        </ul>
+        <form class="navbar-form navbar-left">
+          <div class="form-group">
+            <input type="text" class="form-control" placeholder="Search">
+          </div>
+          <button type="submit" class="btn btn-default">Submit</button>
+        </form>
+
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  --->
+
+
+
+  <script src="js/jquery-1.12.0.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+
+</body>
+</html>
